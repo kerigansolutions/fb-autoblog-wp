0cf5b8ba41f6ff96ebb0ad2f12eee5ea4f8b5df6
Bryan Baird <23462829+programmer-man@users.noreply.github.com>
2022-11-18 06:59:12 -0600

    adjust event sync number

--- src/WP/FacebookEvent.php
+++ src/WP/FacebookEvent.php
@@ -100,7 +100,7 @@ class FacebookEvent extends FacebookObject {
   public function sync ($request)
   {
     $num = $request->get_param('num');
-    $this->getRemote($num ?? 999);
+    $this->getRemote($num ?? 200);
   }
 
 }
